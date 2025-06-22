0be91e0c7f30364cd85b5c0b7d89c279ecebb421
Claudio Puggioni <76516704+ClaudioPuggioni@users.noreply.github.com>
2025-06-22 08:13:12 +0700

    initial

---
+++ src/models.rs
@@ -0,0 +1,30 @@
+pub enum Status {
+    // TODO: add fields (make sure the fields are public)
+}
+
+pub struct Epic {
+    // TODO: add fields (make sure the fields are public)
+}
+
+impl Epic {
+    pub fn new(name: String, description: String) -> Self {
+        // by default the status should be set to open and the stories should be an empty vector
+        todo!()
+    }
+}
+
+pub struct Story {
+    // TODO: add fields (make sure the fields are public)
+}
+
+impl Story {
+    pub fn new(name: String, description: String) -> Self {
+        // by default the status should be set to open
+        todo!()
+    }
+}
+
+pub struct DBState {
+    // This struct represents the entire db state which includes the last_item_id, epics, and stories
+    // TODO: add fields (make sure the fields are public)
+}
