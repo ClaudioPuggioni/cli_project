0d063e2766cde176a3f7ded48cc5d5139148347e
Claudio Puggioni <76516704+ClaudioPuggioni@users.noreply.github.com>
2025-09-18 11:07:15 +0700

    Step 2 complete

--- src/models.rs
+++ src/models.rs
@@ -1,23 +1,28 @@
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
 pub enum Status {
     Open,
     InProgress,
+    Resolved,
     Closed,
 }
 
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
 pub struct Epic {
-    pub id: u16,
+    // pub id: u32,
     pub name: String,
     pub description: String,
     pub status: Status,
-    pub stories: Vec<u16>,
+    pub stories: Vec<u32>,
 }
 
 impl Epic {
-    pub fn new(name: String, description: String, id: u16) -> Self {
+    // pub fn new(id: u32, name: String, description: String) -> Self {
+    pub fn new(name: String, description: String) -> Self {
         Epic {
-            id,
+            // id,
             name,
             description,
             status: Status::Open,
@@ -26,17 +31,19 @@ impl Epic {
     }
 }
 
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
 pub struct Story {
-    pub id: u16,
+    // pub id: u32,
     pub name: String,
     pub description: String,
     pub status: Status,
 }
 
 impl Story {
-    pub fn new(name: String, description: String, id: u16) -> Self {
+    // pub fn new(id: u32, name: String, description: String) -> Self {
+    pub fn new(name: String, description: String) -> Self {
         Story {
-            id,
+            // id,
             name,
             description,
             status: Status::Open,
@@ -44,14 +51,15 @@ impl Story {
     }
 }
 
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
 pub struct DBState {
-    pub last_item_id: u16,
-    pub epics: HashMap<u16, Epic>,
-    pub stories: HashMap<u16, Story>,
+    pub last_item_id: u32,
+    pub epics: HashMap<u32, Epic>,
+    pub stories: HashMap<u32, Story>,
 }
 
 impl DBState {
-    pub fn new() -> DBState {
+    pub fn new() -> Self {
         DBState {
             last_item_id: 0,
             epics: HashMap::new(),
@@ -61,15 +69,19 @@ impl DBState {
 
     pub fn create_epic(&mut self, name: String, description: String) {
         self.last_item_id += 1;
-        let new_id = self.last_item_id;
-        let new_epic = Epic::new(name, description, new_id);
-        self.epics.insert(new_id, new_epic);
+        // let id = self.last_item_id;
+        // let new_epic = Epic::new(id, name, description);
+        // self.epics.insert(id, new_epic);
+        let new_epic = Epic::new(name, description);
+        self.epics.insert(self.last_item_id, new_epic);
     }
 
     pub fn create_story(&mut self, name: String, description: String) {
         self.last_item_id += 1;
-        let new_id = self.last_item_id;
-        let new_story = Story::new(name, description, new_id);
-        self.stories.insert(new_id, new_story);
+        // let id = self.last_item_id;
+        // let new_story = Story::new(id, name, description);
+        // self.stories.insert(id, new_story);
+        let new_story = Story::new(name, description);
+        self.stories.insert(self.last_item_id, new_story);
     }
 }
